a864eb32c5b8acb61fe109cd8e2253b5a1993ab7
bockdung <slzxc373@gmail.com>
2018-12-17 21:11:38 +0900

    1차 작업 박스생성 , 도착 등 다시 수정할 것 많음

---
+++ Assets/Script/LHJ/Floor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {       
+        
+        if (other.tag=="Box")
+        {
+            GameObject parent = other.transform.parent.gameObject;
+            InGameManger.instance.totalbox++;
+            other.GetComponent<Collider>().enabled = false;
+            Destroy(parent, 1f);
+        }
+    }
+}
